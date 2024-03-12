eaec96c981298f8490ba9cc92e40137630d4240a
ahad1512 <ahadk747@gmail.com>
2024-03-12 09:27:21 +0530

    Completed

---
+++ src/main/java/com/example/rolebasedaccesscontrolassignment/RoleBasedAccessControlAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.rolebasedaccesscontrolassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedAccessControlAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleBasedAccessControlAssignmentApplication.class, args);
+	}
+
+}
